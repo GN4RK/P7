56e1a7828caef479b3b283522b6cfb858f353f6c
Yoann Leonard <yoann.leonard@gmail.com.com>
2022-08-22 15:58:40 +0200

    added validator

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -5,6 +5,7 @@ namespace App\Entity;
 use App\Repository\UserRepository;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: UserRepository::class)]
 class User
@@ -17,14 +18,18 @@ class User
 
     #[ORM\Column(length: 255)]
     #[Groups(["getUsers"])]
+    #[Assert\NotBlank(message: "Username is missing")]
     private ?string $username = null;
 
     #[ORM\Column(length: 255)]
     #[Groups(["getUsers"])]
+    #[Assert\NotBlank(message: "email is missing")]
+    #[Assert\Email(message: "email must be valid")]
     private ?string $email = null;
 
     #[ORM\Column(length: 255)]
     #[Groups(["getUsers"])]
+    #[Assert\NotBlank(message: "password is missing")]
     private ?string $password = null;
 
     #[ORM\ManyToOne(inversedBy: 'users')]
